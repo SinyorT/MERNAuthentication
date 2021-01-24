c870a48c46a1f34a37402d90eb74ed5db6562868
Sinyor <abdurahmanov.togrul@gmail.com>
2021-01-24 18:40:21 +0300

    setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MERNAuthentication
+npx create-react-app client
+
+npm i express mongoose dotenv cors cookie-parser bcrypt jsonwebtoken cloudinary express-fileupload googleapis node-fetch nodemailer
+ 
+#client folder 
+npm i axios react-router-dom redux react-redux
+
+
+
---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+//Connect to mongodb
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+},err=>{
+    if(err) throw err
+    console.log("Connected to mongodb")
+});
+
+
+// app.use('/', (req, res, next) => {
+//     res.json({ msg: "Hello Everyonen!!!" })
+// })
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+})
