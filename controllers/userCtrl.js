b70436cb696eddc7fe26dcafccfaacadec750683
Sinyor <abdurahmanov.togrul@gmail.com>
2021-01-25 00:30:26 +0300

    midddleware and reset password

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -113,6 +113,21 @@ const userCtrl = {
             return res.status(500).json({msg: err.message})
         }
     },
+    resetPassword: async (req, res) => {
+        try {
+            const {password} = req.body
+          
+            const passwordHash = await bcrypt.hash(password, 12)
+             console.log(req.user )
+            await Users.findOneAndUpdate({_id: req.user.id}, {
+                password: passwordHash
+            })
+
+            res.json({msg: "Password successfully changed!"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },  
 }
 
 
